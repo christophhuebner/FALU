d4811b97626f87e834e37ddc8d7b08d9323c53be
Mona Lisa <mona.lisa@lisa.com>
2025-08-07 11:12:53 +0200

    killed parametrization

--- ALU.sv
+++ ALU.sv
@@ -1,20 +1,31 @@
 module ALU #(
-    parameter WIDTH = 8
+
 )(
-    input wire [(WIDTH*2)-1:0] input, // First operand
+    input wire [15:0] in, // First operand
     input  wire [3:0]       op,   // Operation selector
-    output reg  [(WIDTH*2)-1:0] result,
+    output reg  [15:0] result,
     output reg              zero, // Zero flag
     output reg              carry, // Carry flag
     output reg              busy
 );
+    integer i;
 
     // ALU logic implementation here
     always_comb begin : ALU_ops
+    carry = 0;
+    i = 0;
         case(op)
-        4'b0000:
-        // Add more operations as needed
+
+        //mac
+        4'b1001:
+        carry,result = in[15:12] * in[11:8] + in[7:0];
+        //sort
+        4'b1010:
+        //hd/hw
+        4'b1111: 
+        result = in[15] & in[7] + in[14] & in[6] + in[13] & in[5] + in[12] & in[4] + in[11] & in[3] + in[10] & in[2] + in[9] & in[1] + in[8] & in[0];
         default: result = 0;
+
     endcase
     end
 endmodule
