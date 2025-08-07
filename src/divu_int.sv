b3ca9c20ed06312de8310cbbfd48e6eb09c46688
christophhuebner <christophhuebner@online.de>
2025-08-07 17:38:04 +0200

    possible fix

--- src/divu_int.sv
+++ src/divu_int.sv
@@ -51,7 +51,7 @@ module divu_int ( // 8 of numbers in bits
                 b1 <= divisor_abs; // use absolute value of divisor for copy
                 {acc, quo} <= {{8{1'b0}}, dividend_abs, 1'b0};  // initialize calculation
             end
-        end else if (busy) begin
+        end else if (busy & !start) begin
             if (i == 8-1) begin  // we're done
                 busy <= 0;
                 done <= 1;
