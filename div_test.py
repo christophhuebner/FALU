b200a811f21638dc6241c48a8bc2be9c3c9192c1
Amrit Sharma Poudel <a.sharmapoudel@uni-luebeck.de>
2025-08-07 19:52:14 +0200

    Full div test

--- div_test.py
+++ div_test.py
@@ -15,16 +15,16 @@ async def test_division(dut):
     dut.rst.value = 0b0
     await ClockCycles(dut.clk, 2, rising=True)
 
-    dut.a.value = 9
-    dut.b.value = 2
-    dut.start.value = 0b1
-    await ClockCycles(dut.clk, 1, rising=True)
-    dut.start.value = 0b0
+    for a in range (256):
+        for b in range (256):
+            dut.a.value = a
+            dut.b.value = b
 
-    while (dut.busy.value == 0b1):
-        dut._log.info(f"Waiting {dut.valid.value}")
-    dut._log.info(f"Result Valid: {dut.valid.value} quotient: {dut.val.value.integer} remainder: {dut.rem.value.integer}")
+            dut.start.value = 0b1
+            await ClockCycles(dut.clk, 1, rising=True)
+            dut.start.value = 0b0
 
-
-
-    
+            while (dut.busy.value == 0b1):
+                await ClockCycles(dut.clk, 1, rising=True)
+                #dut._log.info(f"Waiting {dut.valid.value}")
+            dut._log.info(f"Result a: {a} b: {b} quotient: {dut.val.value.integer} remainder: {dut.rem.value.integer}")
