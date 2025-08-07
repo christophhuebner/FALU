937e7fe68c9b5841890739ff95b757cf9fd57942
Amrit Sharma Poudel <a.sharmapoudel@uni-luebeck.de>
2025-08-07 14:07:42 +0200

    Hamming distance edited/tested

--- ALU.sv
+++ ALU.sv
@@ -35,8 +35,14 @@ module ALU (
         end
         //hd/hw
         4'b1111: 
-        result = data_in[15] & data_in[7] + data_in[14] & data_in[6] + data_in[13] & data_in[5] + data_in[12] & data_in[4] + data_in[11] & data_in[3] + data_in[10] & data_in[2] + data_in[9] & data_in[1] + data_in[8] & data_in[0];
-
+        result = (data_in[15] ^ data_in[7]) +
+                (data_in[14] ^ data_in[6]) +
+                (data_in[13] ^ data_in[5]) +
+                (data_in[12] ^ data_in[4]) +
+                (data_in[11] ^ data_in[3]) +
+                (data_in[10] ^ data_in[2]) +
+                (data_in[9]  ^ data_in[1]) +
+                (data_in[8]  ^ data_in[0]);
 
         //ADD 
         4'b0000:
