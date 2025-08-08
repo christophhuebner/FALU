c3c0a4c2b456ac1879862cff9c1fe02eb6cf79c2
Amrit Sharma Poudel <a.sharmapoudel@uni-luebeck.de>
2025-08-08 09:41:37 +0200

    Test

--- div_test.py
+++ div_test.py
@@ -15,16 +15,28 @@ async def test_division(dut):
     dut.rst.value = 0b0
     await ClockCycles(dut.clk, 2, rising=True)
 
-    for a in range (256):
-        for b in range (256):
-            dut.a.value = a
-            dut.b.value = b
+    a = 255
+    b = 255
 
-            dut.start.value = 0b1
-            await ClockCycles(dut.clk, 1, rising=True)
-            dut.start.value = 0b0
+    dut.a.value = a
+    dut.b.value = b
 
-            while (dut.busy.value == 0b1):
-                await ClockCycles(dut.clk, 1, rising=True)
+    dut.start.value = 0b1
+    await ClockCycles(dut.clk, 1, rising=True)
+    dut.start.value = 0b0
+
+    while (dut.busy.value == 0b1):
+        await ClockCycles(dut.clk, 1, rising=True)
                 #dut._log.info(f"Waiting {dut.valid.value}")
-            dut._log.info(f"Result a: {a} b: {b} quotient: {dut.val.value.integer} remainder: {dut.rem.value.integer}")
+    dut._log.info(f"Result1 a: {a} b: {b} quotient: {dut.val.value.integer} remainder: {dut.rem.value.integer}")
+
+    dut.a.value = a
+    dut.b.value = b
+    dut.start.value = 0b1
+    await ClockCycles(dut.clk, 1, rising=True)
+    dut.start.value = 0b0
+
+    while (dut.busy.value == 0b1):
+        await ClockCycles(dut.clk, 1, rising=True)
+                #dut._log.info(f"Waiting {dut.valid.value}")
+    dut._log.info(f"Result2 a: {a} b: {b} quotient: {dut.val.value.integer} remainder: {dut.rem.value.integer}")
