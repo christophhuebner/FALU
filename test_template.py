404a16a6ffbaa1bd3966a4eeded0de44e089dc6a
Amrit Sharma Poudel <a.sharmapoudel@uni-luebeck.de>
2025-08-08 17:02:28 +0200

    new tmplate

--- test_template.py
+++ test_template.py
@@ -10,6 +10,7 @@ async def test_falu(dut):
     i2c_clk = Clock(dut.ui_in[2], 1, "ns")
     await cocotb.start(i2c_clk.start())
     
+    await ClockCycles(dut.ui_in[2], 2, rising=True)
     # Reset interface
     dut.rst_n.value = 0b1
     await ClockCycles(dut.ui_in[2], 2, rising=True)
@@ -27,47 +28,42 @@ async def test_falu(dut):
 
     # start writing data
     dut.ui_in[0].value = 0b1
-    await ClockCycles(dut.ui_in[2], 1, rising=True)
+    await ClockCycles(dut.ui_in[2], 2, rising=True)
     dut.ui_in[0].value = 0b0
 
     # Write data to  
-    ops = 0b0010
+    ops = 0b0100
     a = 0b11011101
-    b = 0b01001000
+    b = 0b11001000
     #await ClockCycles(dut.ui_in[2], 1, rising=True)
     data_in = (ops << 16) | (a << 8) | b
 
     for i in range(20):
         dut.ui_in[3].value = (data_in >> 19-i) & 0b1
-        await ClockCycles(dut.ui_in[2], 1, rising=True)
+        # await ClockCycles(dut.ui_in[2], 1, rising=True)
+        await ClockCycles(dut.clk, 1, rising=True)
         
-    await ClockCycles(dut.ui_in[2], 2, rising=True)
+
+    await ClockCycles(dut.clk, 2, rising=True)
 
 
     # Write to slave
-    await ClockCycles(dut.ui_in[2], 1, rising=True)
+    await ClockCycles(dut.clk, 2, rising=True)
     dut.ui_in[1].value = 0b1
-    await ClockCycles(dut.ui_in[2], 1, rising=True)
+    await ClockCycles(dut.clk, 2, rising=True)
 
     # start writing data
-    dut.ui_in[0].value = 0b0001
-    await ClockCycles(dut.ui_in[2], 1, rising=True)
-    dut.ui_in[0].value = 0b0000
-
-    # Write data to  
-    #ops = 0b0011
-    #a = 0b00000111
-    #b = 0b01001010
-    #await ClockCycles(dut.ui_in[2], 1, rising=True)
-    #data_in = (ops << 16) | (a << 8) | b
+    dut.ui_in[0].value = 0b1
+    await ClockCycles(dut.clk, 2, rising=True)
+    dut.ui_in[0].value = 0b0
+    dut._log.info(f"--------")
     x = ""
-    for i in range(18):
+    for i in range(17):
        x += str(dut.uo_out[0].value.binstr)
-       await ClockCycles(dut.ui_in[2], 1, rising=True)
+       # dut._log.info(f"{dut.i2c_inst.}")
+       await ClockCycles(dut.clk, 1, rising=True)
     
     dut._log.info(x)
-    await ClockCycles(dut.ui_in[2], 2, rising=True)
-
 
     
 
