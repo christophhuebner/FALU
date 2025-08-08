03e46ca5ac348e4298cca0837fcfc5e650669cf1
christophhuebner <christophhuebner@online.de>
2025-08-08 13:16:59 +0200

    changed interface

--- src/heichips25_template.sv
+++ src/heichips25_template.sv
@@ -6,12 +6,8 @@
 `default_nettype none
 
 module heichips25_template (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire [3:0] ui_in,    // Dedicated inputs
+    output wire  uo_out,   // Dedicated outputs
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -22,7 +18,7 @@ FALU_top falutop (
     .wr(ui_in[1]),     // Write signal from ui_in[1]
     .i2c_clk(ui_in[2]),  // I2C clock input
     .i2c_sda_i(ui_in[3]), // I2C data input
-    .i2c_sda_o(uo_out[0]) // I2C data output
+    .i2c_sda_o(uo_out) // I2C data output
 );
 
 
