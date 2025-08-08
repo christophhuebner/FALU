d04b2b23beac01285a3139614784dfb28ca4e7d6
christophhuebner <christophhuebner@online.de>
2025-08-08 14:10:33 +0200

    template adapted

--- src/heichips25_template.sv
+++ src/heichips25_template.sv
@@ -420,10 +420,10 @@ endmodule
 module I2C_Controller (
     input  wire scl,
     input  wire sda_i,
-    input reset,
-    input[15:0] result,
-    input start,
-    input w_r,
+    input wire reset,
+    input reg [15:0] result,
+    input wire start,
+    input wire w_r,
     output reg [15:0] data_out, 
     output  reg [3:0]  op,
 	output  reg sda_o
@@ -433,9 +433,9 @@ module I2C_Controller (
     reg [1:0] state = 0;
 	reg [19:0] data_in = 0;
    
-	
+
 	always @(posedge scl) begin
-		if (reset ==1) begin
+		if (reset == 1) begin
 			state <= 0;
 			counter <= 0;
 			data_in <= 0;
@@ -448,32 +448,29 @@ module I2C_Controller (
                         state <= 1;
                     end
                     else begin
-                        counter <= 15;
+                        counter <= 16;
                     state <= 2;
                     end
                   end
 				end
-				
 				1: begin
 					data_in[counter] <= sda_i;
 					if(counter == 0) begin
 						state <= 3;
 					end else counter <= counter - 1;
 				end
-						
 				2: begin
 					if(counter == 0) state <= 0;
-					else begin counter <= counter - 1;
-                    sda_o <= result[counter];
+					else begin 
+                    sda_o <= result[counter-1];
+					counter <= counter - 1;
                 end	
                 end
                   3: begin
 					state <= 0;	
                    op <= data_in[19:16];
                     data_out <= data_in[15:0];
-            	end
-				
-				
+            	end				
 			endcase
 		end
 	end
