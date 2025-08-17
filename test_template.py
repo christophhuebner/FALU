20204b0c368e29c462b81c1d144a5ecec52f349c
Amrit Sharma Poudel <a.sharmapoudel@uni-luebeck.de>
2025-08-17 14:38:50 +0200

    Testbench cleanup

--- test_template.py
+++ test_template.py
@@ -4,82 +4,56 @@ from cocotb.clock import Clock
 
 @cocotb.test()
 async def test_falu(dut):
-    """ Test template with data """
-    c = Clock(dut.clk, 1, "ns")
-    await cocotb.start(c.start())
+    """Testbench for writing to and reading from the FALU interface."""
+    
+    # Start main clock
+    main_clk = Clock(dut.clk, 1, "ns")
+    await cocotb.start(main_clk.start())
+    
+    # Start I2C clock
     i2c_clk = Clock(dut.ui_in[2], 1, "ns")
     await cocotb.start(i2c_clk.start())
     
+    # Reset DUT
     await ClockCycles(dut.ui_in[2], 2, rising=True)
-    # Reset interface
-    dut.rst_n.value = 0b0
+    dut.rst_n.value = 0
     await ClockCycles(dut.ui_in[2], 2, rising=True)
-    dut.rst_n.value = 0b1
-    
-    # Master will was von dir!
-    #dut.ui_in[1].value = 0b0
-    #await ClockCycles(dut.ui_in[2], 1, rising=True)
-    #dut.ui_in[1].value = 0b0
+    dut.rst_n.value = 1
 
-    # Write to slave
+    # Initiate a write operation
     await ClockCycles(dut.ui_in[2], 1, rising=True)
-    dut.ui_in[1].value = 0b0
-    await ClockCycles(dut.ui_in[2], 1, rising=True)
-
-    # start writing data
-    dut.ui_in[0].value = 0b1
+    dut.ui_in[1].value = 0  # Write mode
+    dut.ui_in[0].value = 1  # Raise start signal
     await ClockCycles(dut.ui_in[2], 2, rising=True)
-    dut.ui_in[0].value = 0b0
+    dut.ui_in[0].value = 0  # Lower start signal
 
-    # Write data to  
+    # Prepare data: [opcode(4 bits), a(8 bits), b(8 bits)]
     ops = 0b0100
     a = 0b11011101
     b = 0b11001000
-    #await ClockCycles(dut.ui_in[2], 1, rising=True)
     data_in = (ops << 16) | (a << 8) | b
 
+    # Send data serially over ui_in[3]
     for i in range(20):
-        dut.ui_in[3].value = (data_in >> 19-i) & 0b1
-        # await ClockCycles(dut.ui_in[2], 1, rising=True)
+        dut.ui_in[3].value = (data_in >> (19 - i)) & 0b1
         await ClockCycles(dut.clk, 1, rising=True)
-        
 
-    await ClockCycles(dut.clk, 2, rising=True)
+    # Initiate a read operation
+    await ClockCycles(dut.clk, 1, rising=True)
+    dut.ui_in[1].value = 1  # Read mode
+    dut.ui_in[0].value = 1  # Raise start signal
+    await ClockCycles(dut.clk, 1, rising=True)
+    dut.ui_in[0].value = 0  # Lower start signal
+    dut.ui_in[1].value = 0  # Clear read signal
 
-
-    # Write to slave
-    await ClockCycles(dut.clk, 2, rising=True)
-    dut.ui_in[1].value = 0b1
-    await ClockCycles(dut.clk, 2, rising=True)
-
-    # start reading data
-    dut.ui_in[0].value = 0b1
-    await ClockCycles(dut.clk, 2, rising=True)
-    dut.ui_in[0].value = 0b0
-
-    dut._log.info(f"--------")
-    x = ""
+    # Capture output data from DUT
+    dut._log.info("--------")
+    output_data = ""
     for i in range(17):
-       x += str(dut.uo_out[0].value.binstr)
-       # dut._log.info(f"{dut.i2c_inst.}")
-       await ClockCycles(dut.clk, 1, rising=True)
+        output_data += str(dut.uo_out[0].value.binstr)
+        await ClockCycles(dut.clk, 1, rising=True)
     
-    dut._log.info(x)
+    dut._log.info(output_data)
 
+    # Wait a few cycles before ending test
     await ClockCycles(dut.clk, 10, rising=True)
-
-
-    
-
-    
-
-
-
-
-
-
-    
-
-
-
-
